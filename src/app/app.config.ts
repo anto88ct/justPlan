bbe164ef2946ce3cff797aa91e853dab3c838b2d
antodarr <antonio.darrigo@seedma.it>
2026-06-09 15:01:01 +0200

    feat(pwa): register service worker in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -13,5 +14,9 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideTranslateService({ defaultLanguage: 'it' }),
     provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
